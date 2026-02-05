89c40295674499f5592132f2b2f8dc5d99fb8137
Anthony Dake <adakemusic@gmail.com>
2026-02-05 17:50:07 -0500

    Move projects preview down 2in

--- app/projects/page.client.tsx
+++ app/projects/page.client.tsx
@@ -240,7 +240,7 @@ export default function ProjectsIndexClient() {
           {hoverCapable && (
             <aside className="hidden lg:block lg:pl-10" aria-label="Project preview">
               {/* Center within the visible page area (account for the 56px fixed header). */}
-              <div className="sticky top-[calc(50%+28px)] -translate-y-1/2">
+              <div className="sticky top-[calc(50%+28px+2in)] -translate-y-1/2">
                 <PreviewPanel
                   current={previewCurrent}
                   next={previewNext}
@@ -375,4 +375,3 @@ function PreviewMedia({ preview, onReady }: { preview: ProjectPreview; onReady?:
     />
   );
 }
-
