e5752f11f47459b7d7f5faa17bd0ce606089425c
Anthony Dake <adakemusic@gmail.com>
2026-05-02 15:21:33 -0400

    Fix drum emoji centering, add tagline, drum favicon
    - Center drum emoji in home nav button with flexbox
    - Remove hover underline from home button
    - Add 'Touring Drummer | Session Drummer' tagline under Architect of Sound
    - Replace favicon with drum emoji SVG

--- app/components/HomeMark.tsx
+++ app/components/HomeMark.tsx
@@ -1,10 +1,22 @@
-type HomeMarkProps = {
+interface HomeMarkProps {
   className?: string;
-};
+}
 
 export default function HomeMark({ className }: HomeMarkProps) {
   return (
-    <span aria-hidden="true" className={className} style={{ lineHeight: 1 }}>
+    <span
+      aria-hidden="true"
+      className={className}
+      style={{
+        lineHeight: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textDecoration: 'none',
+        width: '100%',
+        height: '100%',
+      }}
+    >
       🥁
     </span>
   );
